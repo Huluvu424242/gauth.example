b84d0a1942d6a065219a10f03793c7f55ed74ddb
FunThomas424242 <funthomas424242@gmail.com>
2018-05-12 12:16:35 +0200

    codacy issues fixed

--- src/test/com/github/funthomas424242/examples/gauth/ConsoleAuthenticationTest.java
+++ src/test/com/github/funthomas424242/examples/gauth/ConsoleAuthenticationTest.java
@@ -3,15 +3,23 @@ package com.github.funthomas424242.examples.gauth;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
-import java.io.StringReader;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
-class ConsoleAuthenticationTest {
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ConsoleAuthenticationTest {
 
     /**
      * Bitte den Test nicht als Vorbild nehmen - er ist Quark.
      */
     @Test
-    void anmeldungVerweigertTotalSimple() {
+    public void anmeldungVerweigertTotalSimple() {
+
+
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        final PrintStream ps = new PrintStream(baos);
+        System.setOut(ps);
 
         // ein definitiv unglültiger Kode, da zu lang (zumindest für den aktuellen GoogleAuthenticator in 2018).
         final byte[] passCode = "12343434".getBytes();
@@ -20,5 +28,7 @@ class ConsoleAuthenticationTest {
 
         final ConsoleAuthentication authExample = new ConsoleAuthentication();
         authExample.main(null);
+
+        assertTrue(baos.toString().contains("Zugang verweigert!"));
     }
 }
